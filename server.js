31c60aefd587428eccbd96b7ec1bfff1a4e428a2
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-10-30 17:19:51 -0700

    init files

--- server.js
+++ server.js
@@ -1,67 +1,25 @@
-const http = require("http");
-const fs = require("fs");
 
-const PORT = process.env.PORT || 8080;
-
-const server = http.createServer(handleRequest);
-
-function handleRequest(req, res) {
-
-    // Capture the url the request is made to
-    const path = req.url;
-
-    // Depending on the URL, display a different HTML file.
-    switch (path) {
-
-        case "/":
-
-        case "/contact":
-
-        case "/appointment":
-
-        case "/files":
-
-        case "/survey":
-            // return displayMovies(path, req, res);
-            return serveHTML(`${path}.html`, res);
+const express = require("express");
+require('dotenv').config();
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
-        default:
-            return serveHTML("/index.html", res);
-    }
-    // Here we use the fs package to read our index.html file
 
-
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-
-const serveHTML = (filePath, res) => {
-    return fs.readFile(__dirname + filePath, (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-type": "text/html" });
-        res.end(data);
-    })
-}
-
-function displayRoot(url, req, res) {
-
-    fs.readFile(__dirname + "/index.html", function (err, data) {
-
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-    });
-}
+// Add routes, both API and view
+app.use(routes);
 
 
-function display404(url, req, res) {
-    const myHTML = "<html>" +
-        "<body><h1>404 Not Found </h1>" +
-        "<p>The page you were looking for: " + url + " can not be found</p>" +
-        "</body></html>";
 
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end(myHTML);
-}
-
-server.listen(PORT, function () {
-    console.log("Server is listening on PORT: " + PORT);
-});
+// Start the API server
+app.listen(PORT, function () {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
