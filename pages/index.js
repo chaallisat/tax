e574dfb483a5341733aea2f13013aceb3303edb7
ChaA'llisa Taylor <chaallisat@yahoo.com>
2020-01-22 19:36:04 -0700

    added header and footer to home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 
-// import Layout from '../components/MyLayout';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import Link from 'next/link';
 
 
@@ -8,9 +9,11 @@ export default function Tax() {
   return (
     <div>
       <h1 className="title">Loranda Tax Service</h1>
+      <Header/>
       <p className="quotes">A business that make nothing but money is a poor business.</p>
       <p className="author"> ~ Henry Ford</p>
       <p className="body">Tax season can be a time of the year that most people dread. The complicated tax forms, the rude people, and even the thought of doing them wrong can cause many to stress. However, at Loranda Tax Service, our team strive to make this tax year as easy as we possibly can for you. We provide explanations that are easy to understand and we walk you through it with one-on-one guidance.</p>
+      <Footer/>
     </div>
   );
 }
