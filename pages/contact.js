9be61f24dec94c558fd221a2516321a1453f81e6
ChaA'llisa Taylor <chaallisat@yahoo.com>
2020-01-22 20:18:02 -0700

    added dummy info to contact.js

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Link from 'next/link';
+
+
+
+export default function Contact() {
+  return (
+    <div>
+      <h1 className="title">Loranda Tax Service</h1>
+      <Header/>
+      <p className="body">Contact us whenever you want. Or not. I ain't your mamma. LOL!!:P</p>
+      <Footer/>
+    </div>
+  );
+}
