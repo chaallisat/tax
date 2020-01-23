d453a16aec4ac5cf93410c39640f8a1d3d5cc7c7
ChaA'llisa Taylor <chaallisat@yahoo.com>
2020-01-23 10:44:31 -0700

    added general body layout

---
+++ components/MyLayout.js
@@ -0,0 +1,14 @@
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+//   border: '1px solid #DDD',
+  backgroundColor: 'green'
+};
+
+const Layout = () => (
+  <div style={layoutStyle}></div>
+);
+
+export default Layout;
+
